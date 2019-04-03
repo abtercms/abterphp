4d2fc2462521059f0409ce7e15aa597146997c7a
Peter Aba <peter@peteraba.com>
2019-04-03 19:08:35 +0200

    Create email sender bootstrapper

--- src/Framework/abter.php
+++ src/Framework/abter.php
@@ -14,6 +14,7 @@ return [
         Bootstrappers\Cache\CacheBootstrapper::class,
         Bootstrappers\Crypto\CryptoBootstrapper::class,
         Bootstrappers\Databases\SqlBootstrapper::class,
+        Bootstrappers\Email\SenderBootstrapper::class,
         Bootstrappers\Email\TransportBootstrapper::class,
         Bootstrappers\Events\EventDispatcherBootstrapper::class,
         Bootstrappers\Filesystem\FilesystemBootstrapper::class,
