2e0faeaff9b2b54807b0c6f74305f7aa48592f32
Peter Aba <peter@peteraba.com>
2020-01-21 23:34:13 +0100

    Environment config changes

--- config/environment/.env.example.php
+++ config/environment/.env.example.php
@@ -61,17 +61,17 @@ Environment::setVar("REDIS_DATABASE", 0);
 
 /**
  * ----------------------------------------------------------
- * Set the encryption key
- * ----------------------------------------------------------
- */
-Environment::setVar("ENCRYPTION_KEY", "def00000cea4c75b84279f43b56dd90851609717c5d29c215fd2c67f9b1acb0c3b1c5ff8528dbeecf0c1f368baa33284aa36d00b24994872970933e8881802287553ff7d");
-
-/**
- * ----------------------------------------------------------
  * Set the default language
  * ----------------------------------------------------------
  */
 Environment::setVar("DEFAULT_LANGUAGE", "en");
+
+/**
+ * ----------------------------------------------------------
+ * Set the encryption key
+ * ----------------------------------------------------------
+ */
+Environment::setVar("ENCRYPTION_KEY", "def00000cea4c75b84279f43b56dd90851609717c5d29c215fd2c67f9b1acb0c3b1c5ff8528dbeecf0c1f368baa33284aa36d00b24994872970933e8881802287553ff7d");
 
 /**
  * ----------------------------------------------------------
@@ -80,17 +80,8 @@ Environment::setVar("DEFAULT_LANGUAGE", "en");
  */
 Environment::setVar("CRYPTO_FRONTEND_SALT", "");
 Environment::setVar("CRYPTO_ENCRYPTION_PEPPER", "");
-
-/**
- * ----------------------------------------------------------
- * Set directory paths
- * ----------------------------------------------------------
- */
-Environment::setVar("DIR_PRIVATE", "/path/to/root/private");
-Environment::setVar("DIR_PUBLIC", "/path/to/root/public");
-Environment::setVar("DIR_AUTH_CONFIG", "/path/to/root/config/authorization");
-Environment::setVar("DIR_MIGRATIONS", "/path/to/root/resources/migrations");
-Environment::setVar("DIR_LOGS", "/path/to/root/tmp/logs");
+Environment::setVar("CRYPTO_BCRYPT_SALT", "");
+Environment::setVar("CRYPTO_BCRYPT_COST", "");
 
 /**
  * ----------------------------------------------------------
@@ -103,6 +94,25 @@ Environment::setVar("OAUTH2_PUBLIC_KEY_PATH", "");
 Environment::setVar("OAUTH2_ENCRYPTION_KEY", "");
 Environment::setVar("OAUTH2_TOKEN_EXPIRY", "PT1H");
 Environment::setVar("OAUTH2_SECRET_LENGTH", 16);
+
+/**
+ * ----------------------------------------------------------
+ * Cache settings
+ * ----------------------------------------------------------
+ */
+
+Environment::setVar("MODULE_CACHE_KEY", "AbterPhp:Modules");
+
+/**
+ * ----------------------------------------------------------
+ * Set directory paths
+ * ----------------------------------------------------------
+ */
+Environment::setVar("DIR_PRIVATE", "/path/to/root/private");
+Environment::setVar("DIR_PUBLIC", "/path/to/root/public");
+Environment::setVar("DIR_AUTH_CONFIG", "/path/to/root/config/authorization");
+Environment::setVar("DIR_MIGRATIONS", "/path/to/root/resources/migrations");
+Environment::setVar("DIR_LOGS", "/path/to/root/tmp/logs");
 
 /**
  * ----------------------------------------------------------
@@ -120,14 +130,6 @@ Environment::setVar("LOGIN_LOG_IP", "0");
 Environment::setVar("PAGINATION_SIZE_OPTIONS", "10,25,50,200,500");
 Environment::setVar("PAGINATION_SIZE_DEFAULT", "50");
 Environment::setVar("PAGINATION_NUMBER_COUNT", "5");
-
-/**
- * ----------------------------------------------------------
- * Set general website settings
- * ----------------------------------------------------------
- */
-Environment::setVar("WEBSITE_BASE_URL", "https://abtercms.test/");
-Environment::setVar("WEBSITE_SITE_TITLE", "AbterCMS Test Site");
 
 /**
  * ----------------------------------------------------------
@@ -151,6 +153,9 @@ Environment::setVar("ADMIN_DATETIME_FORMAT", "Y-m-d H:i:s");
 Environment::setVar("ADMIN_BASE_PATH", "/admin-iddqd");
 Environment::setVar("ADMIN_LOGIN_PATH", "/login-iddqd");
 Environment::setVar("ADMIN_LOGOUT_PATH", "/logout-iddqd");
+Environment::setVar("DIR_UPLOAD", "/path/to/root/public/editor-file-upload");
+Environment::setVar("UPLOAD_BASE_URL", "https://abtercms.test/editor-file-upload");
+Environment::setVar("UPLOAD_CLIENT_ID", "");
 
 /**
  * ----------------------------------------------------------
@@ -163,22 +168,8 @@ Environment::setVar("API_PROBLEM_BASE_URL", "https://abtercms.com/problem/");
 
 /**
  * ----------------------------------------------------------
- * Cache settings
+ * Set general website settings
  * ----------------------------------------------------------
  */
-
-Environment::setVar("MODULE_CACHE_KEY", "AbterPhp:Modules");
-
-/**
- * ----------------------------------------------------------
- * Oauth2 settings
- * ----------------------------------------------------------
- */
-
-Environment::setVar("OAUTH2_PRIVATE_KEY_PATH", "");
-Environment::setVar("OAUTH2_PRIVATE_KEY_PASSWORD", "");
-Environment::setVar("OAUTH2_PUBLIC_KEY_PATH", "");
-Environment::setVar("OAUTH2_ENCRYPTION_KEY", "");
-Environment::setVar("OAUTH2_TOKEN_EXPIRY", "");
-Environment::setVar("OAUTH2_SECRET_LENGTH", "");
-
+Environment::setVar("WEBSITE_BASE_URL", "https://abtercms.test/");
+Environment::setVar("WEBSITE_SITE_TITLE", "AbterCMS Test Site");
